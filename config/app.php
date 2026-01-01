5b3d18af238c6dd765d21eebf7b45cbefd57d130
ONCC-SN <oncc@senegal.gov.sn>
2026-01-01 19:32:49 +0000

    Add RouteServiceProvider to register web routes

--- config/app.php
+++ config/app.php
@@ -104,6 +104,7 @@ return [
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
+        App\Providers\RouteServiceProvider::class,
 
     ],
 
